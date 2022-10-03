6a3c81f321b67c5da2e45b6f09c7db706216f367
Jade Naughton <jade.naughton@kainos.com>
2022-10-03 13:44:46 +0100

    Removed some unnecessary comments, cleaned up jobdata.js

--- data/jobdata.js
+++ data/jobdata.js
@@ -5,21 +5,19 @@ exports.getJobRoles = async () => {
       const jobResponse = await axios.get('http://localhost:8080/api/job-roles' )
       return jobResponse.data
    } catch (e) {
-      if (e.response) {
+      if (e.response) { // If the API returned a response (good or bad)
          if(e.response.status == 500){
             return new Error('Failed to get roles');
          }
          else if (e.response.status == 400) {
             return new Error('Could not find roles');
          }
-         console.log("There was a response")
-      } else if (e.code = 'ECONNREFUSED') { // Only true if API response not present
+      } else if (e.code = 'ECONNREFUSED') { // If API response not present
          return new Error('Unable to reach API');
-      } else {
+      } else { // All other eventualities
+         console.log("An unknown error occurred while retrieving job roles")
          return new Error('Error while contacting API, please contact site Admin');
       }
-      console.log(e.response.status)
-      //return new Error('Error contacting API, please contact site Admin')
    }
 }
  
