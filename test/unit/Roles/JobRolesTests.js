b4c152a881966b4585974f63326c45b8a115730b
Conor McGee <conor.mcgee@kainos.com>
2022-10-06 10:58:01 +0100

    white spacing removed from tests

--- test/unit/Roles/JobRolesTests.js
+++ test/unit/Roles/JobRolesTests.js
@@ -10,54 +10,39 @@ const jobRoles = {
   job_description: "Builds the software from requirements set by clients",
   name: "Software Engineer"
 }
-
 const failedRolesError = 'Failed to get roles';
 const failedFindRolesError = 'Could not find roles'
 const failedToReachAPIError = 'Unable to reach API'
 const allOtherErrors = 'Error contacting API, please contact site Admin'
 
-
-
 describe('getJobRoles', function () {
 
   it('should return jobs from response', async () => {
     var mock = new MockAdapter(axios);
-
     const data = [jobRoles];
-
     mock.onGet(URL).reply(200, data);
-
     var results = await jobdata.getJobRoles();
-
     expect(results[0]).to.deep.equal(jobRoles)
   })
 
   it('should throw exception when 500 error returned from axios', async () => {
     var mock = new MockAdapter(axios);
-
     mock.onGet(URL).reply(500);
-
     var error = await jobdata.getJobRoles()
-
     expect(error.message).to.equal(failedRolesError)
   })
 
   it('should throw exception when 400 error returned from axios', async () => {
     var mock = new MockAdapter(axios);
-
     mock.onGet(URL).reply(400);
-
     var error = await jobdata.getJobRoles()
-
     expect(error.message).to.equal(failedFindRolesError)
   })
 
   it('should throw exception when axios has no response', async () => {
     var mock = new MockAdapter(axios);
     mock.onGet(URL).timeout();
-
     var error = await jobdata.getJobRoles()
-
     expect(error.message).to.equal(failedToReachAPIError)
   })
 
