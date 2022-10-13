2ffd5fd6f4ef7ddaff94a5335863e2315e70364a
kirsten.williamson <kirsten.williamson@kainos.com>
2022-10-13 15:56:03 +0100

    Fixed a few issues

--- controller/JobsController.js
+++ controller/JobsController.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { render } = require('nunjucks');
 var router = express.Router();
 var jobdata = require('../data/jobdata.js')
 const JobValidator = require('../validator/JobValidator');
@@ -31,8 +32,28 @@ router.get('/job-roles', async (req, res) => {
 });
 
 router.get('/admin/new-role' , async (req, res) => {
+  renderNewRole(req, res)
+});
+
+router.post('/insert-role', async (req, res) => {
+  try {        
+      const id = await jobdata.createRole(req.body)
+      if(id != null) {
+        res.locals.outcome = "Role added successfully"
+        renderNewRole(req, res)
+      }
+      else {
+        res.locals.outcome = "Role can not be added at this time"
+        renderNewRole(req, res)
+      }
+  } catch (e) {
+      res.locals.errormessage = "Failed to submit form"
+      res.render('new_role', req.body)
+  }
+});
+
+async function renderNewRole (req, res) {
   try {
-    //var job = await jobdata.getJobRoleById(req.query)
     var capabilities = await jobdata.getCapabilities();
     var bands = await jobdata.getBands();
     if (capabilities instanceof Error || bands instanceof Error) {
@@ -47,39 +68,12 @@ router.get('/admin/new-role' , async (req, res) => {
     } else {
         let data = {capabilities: capabilities, bands: bands}
         console.log(data)
-      res.render('new_role.html', data)
+        res.render('new_role.html', data)
     }
   } catch (e) {
     res.locals.errormessage = "Failed to retrieve information: " + e;
     res.render('error', req.body)
   }
-});
-
-router.post('/insert-role', async (req, res) => {
-  //let error = JobValidator.isValidRole(req.body)
-  error = null
-
-  console.log(error)
-
-  if (error) {
-      res.locals.errormessage = error
-      return res.render('new_role', req.body) 
-  }
-
-  try {        
-      const id = await jobdata.createRole(req.body)
-      if(id != null) {
-        res.locals.outcome = "Role added successfully"
-        res.render('new_role')
-
-      }
-      else {
-
-      }
-  } catch (e) {
-      res.locals.errormessage = "Failed to submit form"
-      res.render('new_role', req.body)
-  }
-});
+}
 
 module.exports = router;
--- validator/JobValidator.js
+++ validator/JobValidator.js
@@ -3,21 +3,5 @@ module.exports.isValidRole = function (role) {
         return "All fields must have a value";
     }
 
-    if (role.getName.length > 50) {
-        return "Name too long"
-    }
-
-    if (role.getDescription.length > 500) {
-        return "Description too long";
-    }
-
-    if (role.getSpecification.length > 255) {
-        return "Speficiation too long";
-    }
-
-    if (role.getResponsibilities.length > 500) {
-        return "Responsibilities too long";
-    }
-
     return null
 }
