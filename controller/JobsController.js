90140c9dbbbe1250287ed9311a36b8d1dd497599
Conor McGee <conor.mcgee@kainos.com>
2022-10-13 09:58:00 +0100

    UI changes admin page and delete job page

--- controller/JobsController.js
+++ controller/JobsController.js
@@ -6,6 +6,10 @@ router.get('/', async (req, res) => {
   res.redirect('index') 
 });
 
+router.get('/admin/index', async (req, res) => {     
+  res.render('admin') 
+});
+
 router.get('/job-roles', async (req, res) => {
   try {
     var jobs = await jobdata.getJobRoles()
@@ -28,5 +32,28 @@ router.get('/job-roles', async (req, res) => {
     res.render('error', req.body)
   }
 });
+router.get('/delete-role', async (req, res) => {
+  try {
+    var jobs = await jobdata.getJobRoles()
+    if (jobs instanceof Error) {
+      res.locals.errormessage = "Failed to retrieve jobs";
+      res.render('error', req.body)
+    } else if(jobs == null){
+      res.locals.errormessage = "No jobs found";
+      res.render('error', req.body)
+    } else {
+      jobs.forEach(job => {
+        if (job.responsibilities.length = 500) {
+          job.responsibilities = job.responsibilities + "..."
+        }
+      });
+      res.render('deletejob.html', { jobRoles: jobs })
+    }
+  } catch (e) {
+    res.locals.errormessage = "Failed to retrieve jobs: " + e;
+    res.render('error', req.body)
+  }
+});
+
 
 module.exports = router;
